d97fb8249496da2ff5190cffcb2a2b79a42e68b2
James Batt <getjamesbatt@gmail.com>
2018-05-09 16:14:32 +1000

    now picks best matching url rather than first

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { swaggerPathToUrlPattern } from "./util";
+import { swaggerPathToUrlPattern, indexOfMin } from "./util";
 
 const yaml = require('js-yaml');
 const jsonrefs = require('json-ref-lite');
@@ -16,7 +16,24 @@ namespace SwaggerFetchStub {
 
     return async function fetch(url: string, init: any) {
       url = url.split('?')[0].split('#')[0]; // strip the query and hash from the URL
-      const pathIndex = urlPatterns.findIndex((pattern: any) => pattern.match(url) !== null);
+
+      // we want to find the index of the url pattern that matches the requested
+      // url. In the case of multiple matches, we want to pick the match with the
+      // least matching path params.
+      // This is done so that paths such as
+      //   /api/item/:name/:name/
+      // that appear before paths such as
+      //   /api/item/:name/value/
+      // don't stop the latter from being requested.
+      const matchingUrlsPaths = urlPatterns.map((pattern: any) => {
+        const match = pattern.match(url);
+        if (match) {
+          return Object.keys(match).length;
+        }
+        return Infinity;
+      });
+      const pathIndex = indexOfMin(matchingUrlsPaths);
+
       if (pathIndex === -1) {
         console.error(url, init, urlPatterns);
         throw new Error(`swagger-fetch-stub: unknown path ${url}`);
--- src/util.ts
+++ src/util.ts
@@ -10,3 +10,21 @@ export function swaggerPathToUrlPattern(path: string) {
   });
   return new UrlPattern(urlPath);
 }
+
+export function indexOfMin(list: number[]) {
+  if (list.length === 0) {
+    return -1;
+  }
+
+  var min = list[0];
+  var minIndex = 0;
+
+  for (var i = 1; i < list.length; i++) {
+    if (list[i] < min) {
+      minIndex = i;
+      min = list[i];
+    }
+  }
+
+  return minIndex;
+}
